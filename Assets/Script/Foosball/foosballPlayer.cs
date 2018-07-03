03e15e40a57fb8885c4871760ddb02d059c11cd9
Nils Erickson <nils.o.erickson@gmail.com>
2018-07-02 20:43:26 -0700

    Initate Foosball Element
    Started to create the foosball
    Test page created

---
+++ Assets/Script/Foosball/foosballPlayer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foosballPlayer : MonoBehaviour {
+
+    public GameObject Character;
+    public float Speed = 5f;
+    public bool canControl;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(Rotate(Vector3.right, 180, .1f));
+        }
+
+        if (canControl == true)
+        {
+            float h = Input.GetAxis("Horizontal") * Speed;
+
+            Character.transform.Translate(h * Time.deltaTime, 0, 0);
+        }
+    }
+    
+
+    IEnumerator Rotate(Vector3 axis, float angle, float duration = 1.0f)
+    {
+        Quaternion from = transform.rotation;
+        Quaternion to = transform.rotation;
+        to *= Quaternion.Euler(axis * angle);
+
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            transform.rotation = Quaternion.Slerp(from, to, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+        transform.rotation = to;
+    }
+}
--- Assets/Script/pickupController.cs
+++ Assets/Script/pickupController.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class rotator : MonoBehaviour {
+public class pickupController : MonoBehaviour {
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update()
     {
         transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
-	}
+    }
 
     private void OnTriggerEnter(Collider other)
     {
