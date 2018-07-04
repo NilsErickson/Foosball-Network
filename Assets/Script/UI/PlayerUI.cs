0255cd4f15c55b4a9b668666c2370ecb6937d7cc
Nils Erickson <nils.o.erickson@gmail.com>
2018-07-03 23:30:58 -0700

    Updated Bumper Ball
    Added Bumper ball test

---
+++ Assets/Script/UI/PlayerUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerUI : MonoBehaviour {
+
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+
+    // Use this for initialization
+    void Start()
+    {
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/Scenes");
+        scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 30), "Change Scene"))
+        {
+            Debug.Log("Scene2 loading: " + scenePaths[0]);
+            SceneManager.LoadScene(scenePaths[0], LoadSceneMode.Single);
+        }
+    }
+}
